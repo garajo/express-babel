2bea85dda5c0e677080364f94cd0876d9292f517
garajo <garrickajo@gmail.com>
2018-08-01 13:15:34 -0700

    Added watch and inspect.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import app from './app';
 
+console.log('hello')
+
+
 const { PORT = 8080 } = process.env;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); // eslint-disable-line no-console
